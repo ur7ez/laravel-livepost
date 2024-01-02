26aaaa97ec9f9f62d85531dd66a30993de9280d1
ur7ez <ur7ez@ukr.net>
2024-01-02 21:13:01 +0200

    done DB migration

--- database/migrations/2024_01_02_181639_create_comments_table.php
+++ database/migrations/2024_01_02_181639_create_comments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->json('body')->nullable();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
