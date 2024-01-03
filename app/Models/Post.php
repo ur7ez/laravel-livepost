fd5afa2a745df7f11e3d0ba4cc2c44226926a84b
ur7ez <ur7ez@ukr.net>
2024-01-03 21:55:05 +0200

    add DB data models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'body' => 'array',  // body response is in json
+    ];
+
+    public function getTitleUpperCaseAttribute()
+    {
+        return strtoupper($this->title);
+    }
+
+    public function setTitleAttribute(string $value)
+    {
+        return $this->attributes['title'] = strtolower($value);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'post_id');
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'post_user', 'post_id', 'user_id');
+    }
 }
