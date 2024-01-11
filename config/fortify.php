4f271753e79d988b0b33b8e235d1c136b08ff58b
ur7ez <ur7ez@ukr.net>
2024-01-11 22:18:45 +0200

    test Fortify features

--- config/fortify.php
+++ config/fortify.php
@@ -147,9 +147,9 @@ return [
     'features' => [
         Features::registration(),
         Features::resetPasswords(),
-//        // Features::emailVerification(),
-//        Features::updateProfileInformation(),
-//        Features::updatePasswords(),
+        Features::emailVerification(),
+        Features::updateProfileInformation(),
+        Features::updatePasswords(),
 //        Features::twoFactorAuthentication([
 //            'confirm' => true,
 //            'confirmPassword' => true,
